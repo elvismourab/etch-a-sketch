55b1480c2b107318d93e2236e392210f88f7cb0a
elvismourab <216804366+elvismourab@users.noreply.github.com>
2025-12-21 11:58:43 -0300

    feat: add pen effect on hoover

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
-const grid = document.getElementById('grid');
+function createGrids() {
+    const grid = document.getElementById('grid');
 
-for (let i = 1; i <= 256; i++) {
-    const div = document.createElement('div');
-    div.classList.add('square');
-    grid.appendChild(div);
+    for (let i = 1; i <= 256; i++) {
+        const div = document.createElement('div');
+        div.classList.add('square');
+        grid.appendChild(div);
+    };
 }
+
+function createPenEffect() {
+    const squares = document.getElementsByClassName('square');
+    console.log(typeof squares);
+
+    for (let square of squares) {
+        square.addEventListener('mouseenter', () => {
+            square.classList.add('hoovered');
+        })
+
+        square.addEventListener('mouseleave', () => {
+            square.classList.remove('hoovered');
+        })
+    }
+}
+
+createGrids();
+createPenEffect();
